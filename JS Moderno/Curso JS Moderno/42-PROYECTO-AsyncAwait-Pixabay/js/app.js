5c91fa610c7ddca462fd2459f1c79ac818f474a7
NicoAnabalon <nicolas.anabalon.a@gmail.com>
2021-08-10 22:23:09 -0400

    Capitulo 42 completado

--- JS Moderno/Curso JS Moderno/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ JS Moderno/Curso JS Moderno/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -48,19 +48,28 @@ function mostrarAlerta(mensaje) {
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '22854412-cdc81795166f61021d0f9320c';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
+    // fetch(url)
+    //     .then(respuesta => respuesta.json())
+    //     .then(resultado => {
+    //         totalPaginas = calcularPaginas(resultado.totalHits);
+    //         mostrarImagenes(resultado.hits);
+    //     })
+    
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log('error :>> ', error);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
